2c4aab58d5ed2f31eda88a0554d4b229334413f2
sofiaavillarejo <sofiavillarejo.dev@gmail.com>
2024-10-18 11:05:09 +0200

    cambio menurutas

--- src/components/MenuRutas.js
+++ src/components/MenuRutas.js
@@ -6,9 +6,9 @@ export default class MenuRutas extends Component {
       <div>
         <ul id='menurutas'>
           <li><a href='/'>Home</a></li>
-          <li><a href='/tablamultiplicar21'>Tabla multiplicar 21</a></li>
-          <li><a href='/tablamultiplicar66'>Tabla multiplicar 66 </a></li>
-          <li><a href='/tablamultiplicar55'>Tabla multiplicar 55</a></li>
+          <li><a href='/tablamultiplicar/21'>Tabla multiplicar 21</a></li>
+          <li><a href='/tablamultiplicar/66'>Tabla multiplicar 66 </a></li>
+          <li><a href='/tablamultiplicar/55'>Tabla multiplicar 55</a></li>
           <li><a href='/noexisto'>Sin ruta</a></li>
         </ul>
       </div>
